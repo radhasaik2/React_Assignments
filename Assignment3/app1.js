d71e724c607eac043d99c10e3f174624d47a78ce
radhasaik2 <radhasaik.2@gmail.com>
2025-10-31 23:24:09 +0530

    Assignment3 app1.js

--- Assignment3/app1.js
+++ Assignment3/app1.js
@@ -6,10 +6,10 @@ const rootreact = createRoot(htmlelement);
 
 const Header = () => {
   return (
-    <div>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHFI2_qgZYlv5Vxzc0l6h8ZOjC8oMNVVd9TQ&s"></img>
-      <input type='search' placeholder='Search here'></input>
-      <h2>Rapoz</h2>
+    <div className="header">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHFI2_qgZYlv5Vxzc0l6h8ZOjC8oMNVVd9TQ&s" alt="JJK" className="img" />
+      <input type='search' placeholder='Search here' className='search'/>
+      <h2 className="icon">Rapoz</h2>
     </div>
   );
 }
